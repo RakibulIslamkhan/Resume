d676df35892976ae7e4f5e98e78c44e543c4dabc
popcornbd <sales.popcornbd@gmail.com>
2023-03-13 21:24:12 +0600

    'gsap'

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -160,7 +160,7 @@ export default function HeroSection() {
             </a>
           </div>
         </div>
-        <section className="flex px-40 sm:px-5 items-center panel center">
+        <section className="flex px-40 sm:px-5 items-center panel center justify-between">
           <div>
             <div>
               <FadeIn
@@ -175,7 +175,7 @@ export default function HeroSection() {
               <FadeIn
                 vars={{ y: -200, duration: 3, ease: "power2.out", delay: 1 }}
               >
-                <h1 className="text-7xl sm:text-3xl mb-8 font-bold dark:text-gray-900">
+                <h1 className="text-6xl sm:text-3xl mb-8 font-bold dark:text-gray-900">
                   Rakibul Islam Khan. 
                   {/* <span><svg viewBox="0 0 200 50">
                     <text x="50%" y="35%" dy=".35em" text-anchor="middle">
@@ -189,13 +189,13 @@ export default function HeroSection() {
               <FadeIn
                 vars={{ y: -200, duration: 3, ease: "power2.out", delay: 2 }}
               >
-                <h1 className="text-6xl sm:text-3xl font-bold pb-8 text-gray-500 dark:text-gray-500">
+                <h1 className="text-5xl sm:text-3xl font-bold pb-8 text-gray-500 dark:text-gray-500">
                   I build things for the web.
                 </h1>
               </FadeIn>
             </div>
             <div>
-              <p className="text-lg font-light max-w-2xl text-justify mb-12 dark:text-gray-900">
+              <p className="text-lg font-light max-w-xl text-justify mb-12 dark:text-gray-900">
                 I&apos;m a frontend developer specializing in crafting engaging
                 and user-friendly digital experiences. Currently, I&apos;m
                 focused on developing accessible and intuitive products at{" "}
@@ -233,14 +233,14 @@ export default function HeroSection() {
               </a>
             </div>
           </div>
-          {/* <div>
-          <img
-            src="https://i.postimg.cc/Wz3b2frL/ray-so-export-1.png"
-            alt=""
-            className="w-96"
-            ref={imgRef}
-          />
-        </div> */}
+          <div>
+          <Player
+          autoplay
+          loop
+          src="https://assets2.lottiefiles.com/packages/lf20_jjsrh4we.json"
+          style={{ height: "400px", width: "400px",}}
+        ></Player>
+        </div>
         </section>
         <Player
           autoplay
